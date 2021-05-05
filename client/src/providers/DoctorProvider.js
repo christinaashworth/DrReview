c612ac0f67558d8ea7e819e045428a8ba0a0570e
Christina Ashworth <christina.ashworth@ymail.com>
2021-05-05 14:53:07 -0500

    made doctor provider

---
+++ client/src/providers/DoctorProvider.js
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from "react";
+import { UserProfileContext } from './UserProfileProvider';
+
+export const DoctorContext = React.createContext();
+
+export const DoctorProvider = (props) => {
+  const apiUrl = "/api/doctor";
+  const [doctors, setDoctors] = useState([]);
+  const { getToken } = useContext(UserProfileContext);
+
+  const getAllDoctors = () => {
+    return getToken().then((token) =>
+    fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
+      },
+    })
+    .then((res) => res.json())
+    .then(setDoctors))
+  }
+
+  const getDoctorById = (id) => {
+    return getToken().then((token) => 
+      fetch(`${apiUrl}/${id}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        },
+      })
+      .then((res) => res.json()))
+  }
+
+  return (
+    <DoctorContext.Provider value={{doctors, setDoctors, getAllDoctors, getDoctorById}}>
+      {props.children}
+    </DoctorContext.Provider>
+  )
+}
