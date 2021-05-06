9ceceac568972c5cee8aa0574fe8b554db07c4d3
Christina Ashworth <christina.ashworth@ymail.com>
2021-05-06 11:59:37 -0500

    backend crud for reviews works now--created repository and controller

---
+++ DrReview/Controllers/ReviewController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using DrReview.Models;
+using DrReview.Repositories;
+
+namespace DrReview.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IReviewRepository _reviewRepository;
+        public ReviewController(IReviewRepository reviewRepository)
+        {
+            _reviewRepository = reviewRepository;
+        }
+
+        [HttpGet("GetReviewsByDoctor/{id}")]
+        public IActionResult GetReviewsByDoctor(int id)
+        {
+            var review = _reviewRepository.GetReviewsByDoctor(id);
+
+            return Ok(review);
+        }
+
+        [HttpGet("GetReviewsByUser/{id}")]
+        public IActionResult GetReviewsByUser(int id)
+        {
+            var review = _reviewRepository.GetReviewsByUser(id);
+
+            return Ok(review);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var review = _reviewRepository.GetReviewById(id);
+
+            return Ok(review);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Review review)
+        {
+            review.CreateDateTime = DateTime.Now;
+            _reviewRepository.AddReview(review);
+            return CreatedAtAction("Get", new { id = review.Id }, review);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Review review)
+        {
+            _reviewRepository.EditReview(review);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _reviewRepository.DeleteReview(id);
+            return NoContent();
+        }
+    }
+}
--- DrReview/Repositories/ReviewRepository.cs
+++ DrReview/Repositories/ReviewRepository.cs
@@ -1,11 +1,211 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Data;
+using System.Reflection.PortableExecutable;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using DrReview.Models;
+using DrReview.Utils;
 
 namespace DrReview.Repositories
 {
-    public class ReviewRepository
+    public class ReviewRepository : BaseRepository, IReviewRepository
     {
+        public ReviewRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Review> GetReviewsByDoctor(int DoctorId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT r.Id, r.UserProfileId, r.DoctorId, r.Title, r.Content, r.CreateDateTime, up.Username
+                                        FROM Review r
+                                            LEFT JOIN UserProfile up ON r.UserProfileId = up.Id
+                                        WHERE r.DoctorId = @DoctorId
+                                        ORDER BY r.CreateDateTime DESC
+                                        ";
+                    DbUtils.AddParameter(cmd, "DoctorId", DoctorId);
+                    var reader = cmd.ExecuteReader();
+
+                    var reviews = new List<Review>();
+
+                    while (reader.Read())
+                    {
+                        var review = new Review
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                            DoctorId = DbUtils.GetInt(reader, "DoctorId"),
+                            Title = DbUtils.GetString(reader, "Title"),
+                            Content = DbUtils.GetString(reader, "Content"),
+                            CreateDateTime = DbUtils.GetDateTime(reader, "CreateDateTime"),
+                        };
+                        review.UserProfile = new UserProfile()
+                        {
+                            Username = DbUtils.GetString(reader, "Username")
+                        };
+                        reviews.Add(review);
+                    }
+
+                    reader.Close();
+
+                    return reviews;
+                }
+            }
+        }
+
+        public List<Review> GetReviewsByUser(int userProfileId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT r.Id, r.UserProfileId, r.DoctorId, r.Title, r.Content, r.CreateDateTime, up.Username
+                                        FROM Review r
+                                            LEFT JOIN UserProfile up ON r.UserProfileId = up.Id
+                                        WHERE r.UserProfileId = @UserId
+                                        ORDER BY r.CreateDateTime DESC
+                                        ";
+
+                    cmd.Parameters.AddWithValue("UserId", userProfileId);
+                    var reader = cmd.ExecuteReader();
+
+                    var reviews = new List<Review>();
+
+                    while (reader.Read())
+                    {
+                        var review = new Review
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                            DoctorId = DbUtils.GetInt(reader, "DoctorId"),
+                            Title = DbUtils.GetString(reader, "Title"),
+                            Content = DbUtils.GetString(reader, "Content"),
+                            CreateDateTime = DbUtils.GetDateTime(reader, "CreateDateTime"),
+                        };
+                        review.UserProfile = new UserProfile()
+                        {
+                            Username = DbUtils.GetString(reader, "Username")
+                        };
+                        reviews.Add(review);
+                    }
+
+                    reader.Close();
+
+                    return reviews;
+                }
+            }
+        }
+
+        public Review GetReviewById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT r.Id, r.UserProfileId, r.DoctorId, r.Title, r.Content, r.CreateDateTime, up.Username
+                                        FROM Review r
+                                            LEFT JOIN UserProfile up ON r.UserProfileId = up.Id
+                                        WHERE r.Id = @Id";
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    var reader = cmd.ExecuteReader();
+
+                    Review review = null;
+
+                    if (reader.Read())
+                    {
+                        review = new Review
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                            DoctorId = DbUtils.GetInt(reader, "DoctorId"),
+                            Title = DbUtils.GetString(reader, "Title"),
+                            Content = DbUtils.GetString(reader, "Content"),
+                            CreateDateTime = DbUtils.GetDateTime(reader, "CreateDateTime"),
+                        };
+                        review.UserProfile = new UserProfile()
+                        {
+                            Username = DbUtils.GetString(reader, "Username")
+                        };
+                    }
+
+                    reader.Close();
+
+                    return review;
+                }
+            }
+        }
+
+        public void AddReview(Review review)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        INSERT INTO Review (UserProfileId, DoctorId, Title, Content, CreateDateTime)
+                                        OUTPUT INSERTED.ID
+                                        VALUES (@UserProfileId, @DoctorId, @Title, @Content, @CreateDateTime)
+                                        ";
+
+                    cmd.Parameters.AddWithValue("@UserProfileId", review.UserProfileId);
+                    cmd.Parameters.AddWithValue("@DoctorId", review.DoctorId);
+                    cmd.Parameters.AddWithValue("@Title", review.Title);
+                    cmd.Parameters.AddWithValue("@Content", review.Content);
+                    cmd.Parameters.AddWithValue("@CreateDateTime", review.CreateDateTime);
+
+                    review.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public void EditReview(Review review)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        UPDATE Review
+                                            SET Title = @Title
+                                                Content = @Content
+                                        WHERE Id = @Id
+                                        ";
+                    cmd.Parameters.AddWithValue("@Id", review.Id);
+                    cmd.Parameters.AddWithValue("@Title", review.Title);
+                    cmd.Parameters.AddWithValue("@Content", review.Content);
+
+                    cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public void DeleteReview(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        DELETE From Review
+                                        WHERE Id = @Id
+                                        ";
+
+                    DbUtils.AddParameter(cmd, "@Id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
