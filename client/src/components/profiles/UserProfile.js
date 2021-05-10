1a149f2fde165106d088d39e19bd384fcd74af51
Christina Ashworth <christina.ashworth@ymail.com>
2021-05-10 10:42:58 -0500

    working on userProfile

---
+++ client/src/components/profiles/UserProfile.js
@@ -0,0 +1,5 @@
+import React, { useContext, useEffect, useState } from "react";
+import { UserProfileContext } from "../../providers/UserProfileProvider";
+import { useHistory } from 'react-router-dom';
+
+const profilePage = () => {}
